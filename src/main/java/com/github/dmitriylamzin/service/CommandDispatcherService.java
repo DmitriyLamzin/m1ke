56fc686346a062f2ee1d97f2733124be373a6590
Dmitriy Lamzin <dmitriy.lamzin@gmail.com>
2016-10-27 13:32:58 +0600

    CommandDispatcherService and its tests are created

---
+++ src/main/java/com/github/dmitriylamzin/service/CommandDispatcherService.java
@@ -0,0 +1,126 @@
+package com.github.dmitriylamzin.service;
+
+import com.github.dmitriylamzin.service.AppService;
+import com.github.dmitriylamzin.service.BranchService;
+import com.github.dmitriylamzin.service.IntegrationService;
+import org.apache.log4j.Logger;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+
+public class CommandDispatcherService {
+
+    private final Logger log = Logger.getLogger(this.getClass());
+
+    private static final String INIT = "init";
+    private static final String INTEGRATE = "integrate";
+    private static final String SAVE = "save";
+    private static final String CREATE_BRANCH = "create-branch";
+    private static final String GET_BRANCH = "get-branch";
+    private static final String REMOVE_BRANCH = "remove-branch";
+    private static final String QUIT = "quit";
+
+    private static final String[] COMMANDS = {
+            INIT,
+            INTEGRATE,
+            SAVE,
+            CREATE_BRANCH,
+            GET_BRANCH,
+            REMOVE_BRANCH,
+            QUIT
+    };
+
+    private AppService appService;
+    private BranchService branchService;
+    private IntegrationService integrationService;
+
+
+    private boolean exists(String expectedCommand){
+        for (String command : COMMANDS){
+            if (command.equals(expectedCommand)){
+                log.debug("Command: " + expectedCommand + " exists");
+                return true;
+            }
+        }
+        log.debug("Command: " + expectedCommand + "doesn't exist");
+        return false;
+    }
+
+    public ArrayList<String> dispatchCommand(String... args) throws NullPointerException{
+        log.info("Dispatch command with args: " + args);
+        ArrayList<String> response = new ArrayList<>();
+        String command = args[0];
+        String[] commandArgs = new String[0];
+        if (args.length > 1){
+            commandArgs = Arrays.copyOfRange(args, 1, args.length);
+        }
+        if (exists(command)){
+            switch (command){
+                case INIT:{
+                    boolean isInitialized = appService.init();
+                    if (isInitialized){
+                        response.add("m1ke.is.initialized");
+                    }else {
+                        response.add("m1ke.is.not.initializes");
+                    }
+                    break;
+                }case INTEGRATE: {
+                    ArrayList<String> integrateResponse = integrationService.integrate();
+                    if (integrateResponse.size() > 0){
+                        response.add("m1ke.integration.success");
+                        response.addAll(integrateResponse);
+                    }else {
+                        response.add("m1ke.integration.failed");
+                    }
+                    break;
+                }case SAVE:{
+                    String saveCommandResponse = integrationService.save(commandArgs);
+                    response.add("m1ke.save.proceed.status");
+                    response.add(saveCommandResponse);
+                    break;
+                }case CREATE_BRANCH:{
+                    String createBranchResponse = branchService.createBranch(commandArgs);
+                    response.add("m1ke.create.branch.proceed.status");
+                    response.add(createBranchResponse);
+                    break;
+                }case GET_BRANCH:{
+                    String getBranchResponse =  branchService.getBranch(commandArgs);
+                    response.add("m1ke.get.branch.status");
+                    response.add(getBranchResponse);
+                    break;
+                }case REMOVE_BRANCH:{
+                    String removeBranchResponse = branchService.removeBranch(commandArgs);
+                    response.add("m1ke.remove.branch.status");
+                    response.add(removeBranchResponse);
+                    break;
+                }case QUIT:{
+                    boolean isQuit = appService.quit();
+                    if (isQuit){
+                        response.add("m1ke.is.quit");
+                    }else {
+                        response.add("m1ke.is.not.quit");
+                    }
+                    break;
+                }
+
+            }
+        }else {
+            log.info("There is no such command " + command);
+            throw new NullPointerException(command);
+        }
+        return response;
+    }
+
+    public void setAppService(AppService appService) {
+        this.appService = appService;
+    }
+
+    public void setBranchService(BranchService branchService) {
+        this.branchService = branchService;
+    }
+
+    public void setIntegrationService(IntegrationService integrationService) {
+        this.integrationService = integrationService;
+    }
+}
---
+++ src/test/java/com/github/dmitriylamzin/controller/CommandDispatcherServiceTest.java
@@ -0,0 +1,91 @@
+package com.github.dmitriylamzin.controller;
+
+import com.github.dmitriylamzin.service.CommandDispatcherService;
+import com.github.dmitriylamzin.service.AppService;
+import com.github.dmitriylamzin.service.BranchService;
+import com.github.dmitriylamzin.service.IntegrationService;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.mockito.Mock;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
+import static org.mockito.MockitoAnnotations.initMocks;
+
+
+@RunWith(Parameterized.class)
+public class CommandDispatcherServiceTest {
+    private CommandDispatcherService dispatcher = new CommandDispatcherService();
+
+    @Mock
+    private AppService appService;
+    @Mock
+    private BranchService branchService;
+    @Mock
+    private IntegrationService integrationService;
+
+    private String command;
+    private ArrayList<String> expectedResult;
+
+
+    public CommandDispatcherServiceTest(String command, ArrayList<String> expectedResult) {
+        this.command = command;
+        this.expectedResult = expectedResult;
+    }
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> data(){
+        return Arrays.asList(new Object[][]{
+                {"init", new ArrayList<>(Arrays.asList("m1ke.is.initialized"))},
+                {"integrate",
+                        new ArrayList<>(Arrays.asList("m1ke.integration.success", "file1", "file2"))},
+                {"save",
+                        new ArrayList<>(Arrays.asList("m1ke.save.proceed.status", "success"))},
+                {"create-branch",
+                        new ArrayList<>(Arrays.asList("m1ke.create.branch.proceed.status", "branch created"))},
+                {"get-branch",
+                        new ArrayList<>(Arrays.asList("m1ke.get.branch.status", "branch got"))},
+                {"remove-branch",
+                        new ArrayList<>(Arrays.asList("m1ke.remove.branch.status", "branch removed"))},
+                {"quit", new ArrayList<>(Arrays.asList("m1ke.is.not.quit"))},
+
+        });
+    }
+
+    @Before
+    public void setUp(){
+        initMocks(this);
+        this.dispatcher = new CommandDispatcherService();
+        dispatcher.setAppService(appService);
+        dispatcher.setBranchService(branchService);
+        dispatcher.setIntegrationService(integrationService);
+
+        when(appService.init()).thenReturn(true);
+        when(appService.quit()).thenReturn(false);
+        when(branchService.createBranch()).thenReturn("branch created");
+        when(branchService.getBranch()).thenReturn("branch got");
+        when(branchService.removeBranch()).thenReturn("branch removed");
+        when(integrationService.integrate()).thenReturn(
+                new ArrayList<>(Arrays.asList("file1", "file2")));
+        when(integrationService.save()).thenReturn("success");
+
+    }
+
+    @Test
+    public void dispatchCommandTest(){
+        ArrayList<String> result = dispatcher.dispatchCommand(command);
+
+        assertEquals(result, expectedResult);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void dispatchCommandTestThrowsException(){
+        dispatcher.dispatchCommand("NOT EXISTED COMMAND");
+    }
+}
