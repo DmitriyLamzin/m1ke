a9b39452001fff18ee55c0524ca89cb3a035c440
Dmitriy Lamzin <dmitriy.lamzin@gmail.com>
2016-10-28 11:28:08 +0600

    author info was deleted

--- src/main/java/com/github/dmitriylamzin/service/BranchService.java
+++ src/main/java/com/github/dmitriylamzin/service/BranchService.java
@@ -2,9 +2,6 @@ package com.github.dmitriylamzin.service;
 
 import java.io.IOException;
 
-/**
- * Created by Dmitriy on 25.10.2016.
- */
 public interface BranchService {
     String createBranch(String... branchCommandArgs) throws IOException;
     String getBranch(String... branchCommandArgs);
