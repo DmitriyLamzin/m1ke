dad90b88d508652632b5da020f7f844858c7bbde
Dmitriy Lamzin <dmitriy.lamzin@gmail.com>
2016-11-03 11:05:02 +0600

    saveBranch(Branch branch) method was added

--- src/main/java/com/github/dmitriylamzin/service/BranchService.java
+++ src/main/java/com/github/dmitriylamzin/service/BranchService.java
@@ -1,9 +1,12 @@
 package com.github.dmitriylamzin.service;
 
+import com.github.dmitriylamzin.domain.Branch;
+
 import java.io.IOException;
 
 public interface BranchService {
     String createBranch(String... branchCommandArgs);
     String getBranch(String... branchCommandArgs);
     String removeBranch(String... branchCommandArgs);
+    boolean saveBranch(Branch branch);
 }
