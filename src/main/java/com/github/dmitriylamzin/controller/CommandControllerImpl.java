88be355aa4e6e6085361228767e1b3596759ed9c
Dmitriy Lamzin <dmitriy.lamzin@gmail.com>
2016-11-03 11:11:33 +0600

    Logger was added

--- src/main/java/com/github/dmitriylamzin/controller/CommandControllerImpl.java
+++ src/main/java/com/github/dmitriylamzin/controller/CommandControllerImpl.java
@@ -2,11 +2,13 @@ package com.github.dmitriylamzin.controller;
 
 import com.github.dmitriylamzin.service.CommandDispatcherService;
 import com.github.dmitriylamzin.view.View;
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class CommandControllerImpl implements CommandController {
+    private final Logger log = Logger.getLogger(this.getClass());
 
     @Autowired
     View view;
@@ -17,6 +19,7 @@ public class CommandControllerImpl implements CommandController {
         try {
             view.showInfo(commandDispatcherService.dispatchCommand(args));
         }catch (NullPointerException e){
+            log.error(e.getMessage(), e);
             view.showInfo("m1ke.command.not.recognized");
         }
     }
