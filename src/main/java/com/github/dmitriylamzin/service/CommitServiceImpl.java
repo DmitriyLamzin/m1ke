1fb33c8f27653fbe83594ec7996751f0d1d79c7b
Dmitriy Lamzin <dmitriy.lamzin@gmail.com>
2016-11-11 12:27:31 +0600

    CRUD operations for Head was delegated to the HeadRepositoryForFileSystem;

--- src/main/java/com/github/dmitriylamzin/service/CommitServiceImpl.java
+++ src/main/java/com/github/dmitriylamzin/service/CommitServiceImpl.java
@@ -3,6 +3,7 @@ package com.github.dmitriylamzin.service;
 import com.github.dmitriylamzin.domain.Branch;
 import com.github.dmitriylamzin.domain.Commit;
 import com.github.dmitriylamzin.domain.Head;
+import com.github.dmitriylamzin.repository.HeadRepositoryForFileSystem;
 import com.github.dmitriylamzin.service.helper.PathResolver;
 import com.github.dmitriylamzin.view.View;
 import org.apache.log4j.Logger;
@@ -22,21 +23,19 @@ import java.nio.file.Paths;
 public class CommitServiceImpl implements CommitService{
     private final Logger log = Logger.getLogger(this.getClass());
     @Autowired
-    private HeadService headService;
+    private HeadRepositoryForFileSystem headRepositoryForFileSystem;
     @Autowired
     private BranchService branchService;
     @Autowired
     private View view;
 
     public boolean saveCommit(Commit newCommit) {
-        Head head = headService.getHead();
+        Head head = headRepositoryForFileSystem.getHead();
         head.setLastCommitNumber(newCommit.getId());
         Branch branch = head.getCurrentBranch();
         branch.setLastCommit(newCommit);
         saveCommitFile(newCommit);
-        branchService.saveBranch(branch);
-        headService.saveHead(head);
-        return false;
+        return branchService.saveBranch(branch) && headRepositoryForFileSystem.saveHead(head);
     }
 
     public String saveTrackedFile(Commit newCommit, String changedFile) {
@@ -57,33 +56,50 @@ public class CommitServiceImpl implements CommitService{
     @Override
     public boolean checkout(Commit commit) {
         log.info("checkout");
+        if (clearCurrentWorkingDirectory()){
+            return false;
+        }
+        for (String path : commit.getFilePathKeys()){
+            if (!retrieveFile(commit.getFilePaths().get(path))){
+                return false;
+            }
+
+        }
+        return true;
+    }
+
+    private boolean retrieveFile(String path) {
+        Path relativePath = PathResolver.getCurrentWorkingDirectory().relativize(Paths.get(path));
+        log.debug(relativePath);
+        try {
+            Path committedDirectory = PathResolver.getObjectsDirectoryPath().resolve(relativePath);
+            Path createdFile = committedDirectory.resolve(path);
+            Files.createDirectories(Paths.get(path).getParent());
+            Files.copy(createdFile,
+                    Paths.get(path));
+            return true;
+        }catch (AccessDeniedException e){
+            log.error(e.getMessage(), e);
+            view.showInfo("access.is.denied");
+            view.showInfo(e.getMessage());
+            return false;
+        }catch (IOException e) {
+            log.error(e.getMessage(), e);
+            return false;
+        }
+    }
+
+    private boolean clearCurrentWorkingDirectory() {
         try {
             Files.walkFileTree(PathResolver.getCurrentWorkingDirectory(),
                     new DeletingFileVisitor());
+            return true;
         }catch (AccessDeniedException e){
             log.error(e.getMessage(), e);
             view.showInfo("access.is.denied");
             view.showInfo(e.getMessage());
         }catch (IOException e) {
             log.error(e.getMessage(), e);
-        }
-        for (String path : commit.getFilePathKeys()){
-            Path relativePath = PathResolver.getCurrentWorkingDirectory().relativize(Paths.get(path));
-            log.debug(relativePath);
-            try {
-                Path committedDirectory = PathResolver.getObjectsDirectoryPath().resolve(relativePath);
-                Path createdFile = committedDirectory.resolve(commit.getFilePaths().get(path));
-                Files.createDirectories(Paths.get(path).getParent());
-                Files.copy(createdFile,
-                        Paths.get(path));
-            }catch (AccessDeniedException e){
-                log.error(e.getMessage(), e);
-                view.showInfo("access.is.denied");
-                view.showInfo(e.getMessage());
-            }catch (IOException e) {
-                log.error(e.getMessage(), e);
-            }
-
         }
         return false;
     }
